1b556f6cb192946f149c16037c5473f13f5be437
Admin <durotul73@gmail.com>
2024-05-31 23:49:04 +0700

    first commit

---
+++ Jasa Pengiriman/JS/scripts.js
@@ -0,0 +1,8 @@
+// scripts.js
+document.addEventListener("DOMContentLoaded", function () {
+  // Add custom JavaScript here
+  var btn = document.getElementById("btn");
+  btn.addEventListener("click", function () {
+    alert("Button was clicked!");
+  });
+});
